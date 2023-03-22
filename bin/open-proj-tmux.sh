57bef01d77dd4946816528a797caf4ac6630f6f5
Aiden Petersen <aidenp@iastate.edu>
2023-03-22 00:30:45 -0500

    Made session switching work properly

--- bin/open-proj-tmux.sh
+++ bin/open-proj-tmux.sh
@@ -1,11 +1,30 @@
-#!/bin/zsh
-target=$(find ~/Projects -mindepth 1 -maxdepth 1 -type d | fzf)
-base=$(basename $target)
-session_exists=$(tmux ls -F "#{session_name}" | grep $base)
-cd $target
-if [[ -n "$session_exists" ]]; then
-	tmux a -t $base
+#!/bin/bash
+if [[ $# -eq 1 ]]; then
+    selected=$1
 else
-	tmux new -s $base
+    selected=$(find ~/Projects -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+
+if [[ ! -z $TMUX ]]; then
+    tmux switch-client -t $selected_name
+    exit 0
+fi
+
+tmux a -dt $selected_name
